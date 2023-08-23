7437017c73ec06704b6c505ed357753f59534914
Sina Beyki <mrsaher83@gmail.com>
2023-08-23 19:48:19 +0330

    Accordion Animation Created.

--- src/components/QuestionBox/index.jsx
+++ src/components/QuestionBox/index.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 import accordionItems from "./data";
 
 const QuestionBox = () => {
@@ -11,35 +12,40 @@ const QuestionBox = () => {
   return (
     <main>
       <div className='flex flex-col justify-center items-center py-16'>
-        <h1 className='text-white font-semibold text-4xl text-center'>Frequently Asked Question</h1>
+        <h1 className='text-white font-semibold text-4xl text-center'>سوالات متداول</h1>
 
-        {/* Start Accordion */}
         <div className='w-4/5 my-16'>
           {accordionItems.map((item, index) => (
-
             <div key={index}>
-              {/* Start Accordion Button*/}
               <button
                 className='w-full flex justify-between items-center p-4 text-left border-b'
                 onClick={() => toggleAccordion(index)}
               >
-                <span className='m-4 text-white/70 '>{item.question}</span> {/* Question */}
-                <span className='text-white/70'>{activeIndex === index ? "-" : "+"}</span> {/* Plus Shape Accordion */}
+                <span className='m-4 text-white/70'>{item.question}</span>
+                <span className='text-white/70'>{activeIndex === index ? "-" : "+"}</span>
               </button>
-              {/* End Accordion Button*/}
-              <div
-                className={`overflow-hidden transition-all duration-700 ${
-                  activeIndex === index ? "max-h-max" : "max-h-0"
-                }`}
-              >
-                <div className='px-16 flex items-center justify-center leading-loose py-10 text-white bg-orange-600/20 shapeQ'>{item.answer}</div> {/* Question Answer */}
-              </div>
+              <AnimatePresence initial={false}>
+                {activeIndex === index && (
+                  <motion.div
+                    initial='collapsed'
+                    animate='open'
+                    exit='collapsed'
+                    variants={{
+                      open: { height: "auto" },
+                      collapsed: { height: 0 }
+                    }}
+                    transition={{ duration: 0.7, ease: "easeInOut" }}
+                    className='overflow-hidden'
+                  >
+                    <div className='px-16 flex items-center justify-center leading-loose py-10 text-white bg-orange-600/20 shapeQ'>
+                      {item.answer}
+                    </div>
+                  </motion.div>
+                )}
+              </AnimatePresence>
             </div>
-
           ))}
         </div>
-        {/* End Accordion */}
-
       </div>
     </main>
   );
